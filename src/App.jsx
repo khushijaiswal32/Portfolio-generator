35ef44fc8670b6bec6a05d2a2a143ff7a43892b6
khushi jaiswal <khjaiswal8052@gmail.com>
2025-12-14 21:17:35 +0530

    deploy real portfolio content

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,34 @@ import Header from "./components/Header";
 import Builder from "./pages/Builder";
 import PreviewPages from "./pages/PreviewPages"
 export default function App() {
+  const [form , setForm] = useState( () =>{
+    //load saved data if available
+    const saved = localStorage.getItem("portfolio_data");
+    return saved
+    ? JSON.parse(saved)
+    :{
+     template: "template1",   // default
+  name: "",
+  role: "",
+  about: "",
+  skills: [],
+  projects: [],
+  github: "",
+  linkedin: ""
+    };
+  });
+  // //Auto- save to Local Storage whenever form changes
+  useEffect (() =>{
+    localStorage.setItem("portfolio_data" ,JSON.stringify(form));
+  }, [form]);
   return (
-    <div style={{color:"red",fontSize:"30px"}}>hello git</div>
+ 
+    <div>
+      <Header />
+      <Builder  form={form} setForm={setForm} />
+      <PreviewPages form = {form} />
+    </div>
   );
 }
-//   const [form , setForm] = useState( () =>{
-//     //load saved data if available
-//     const saved = localStorage.getItem("portfolio_data");
-//     return saved
-//     ? JSON.parse(saved)
-//     :{
-//      template: "template1",   // default
-//   name: "",
-//   role: "",
-//   about: "",
-//   skills: [],
-//   projects: [],
-//   github: "",
-//   linkedin: ""
-//     };
-//   });
-//   // //Auto- save to Local Storage whenever form changes
-//   useEffect (() =>{
-//     localStorage.setItem("portfolio_data" ,JSON.stringify(form));
-//   }, [form]);
-//   return (
- 
-//     <div>
-//       <Header />
-//       <Builder  form={form} setForm={setForm} />
-//       <PreviewPages form = {form} />
-//     </div>
-//   );
-// }
 
  
