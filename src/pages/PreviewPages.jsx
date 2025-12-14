21fb12a58a3b15715ee245d6e4a6cd714b2c6b2c
khushi jaiswal <khjaiswal8052@gmail.com>
2025-12-14 13:06:05 +0530

    remove github actions workflow(use gh-pages only)

--- src/pages/PreviewPages.jsx
+++ src/pages/PreviewPages.jsx
@@ -1,4 +1,4 @@
-import {downloadPortfolio} from "../utils/downloadHTML";
+import {downloadPortfolio} from "../utils/downloadHtml";
 import React from "react";
 import Template1 from "../templates/Template1";
 import Template2 from "../templates/Template2";
